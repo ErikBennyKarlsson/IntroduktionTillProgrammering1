357aa82eac7587d9a7bdd5493ac121e830e11e7a
ErikBennyKarlsson <117075358+ErikBennyKarlsson@users.noreply.github.com>
2022-11-07 13:46:34 +0100

    Update IntroduktionMain.java

--- src/Introduktion/IntroduktionMain.java
+++ src/Introduktion/IntroduktionMain.java
@@ -14,40 +14,21 @@ public class IntroduktionMain {
         String text = scan.nextLine();
 
         MyUser startCount = new MyUser();
-        MyUser checker = new MyUser();
-        checker.getChecker();
-       boolean myChecker = checker.getChecker();
+        startCount.theCounter(text);
+        while (startCount.getChecker()) {
 
-        while (myChecker==true) {
-
-            startCount.theCounter(text);
             System.out.println("Skriv in ny valfri mening");
             text = scan.nextLine();
-
-
-
-            MyUser textCounter = new MyUser();
-            textCounter.getRows();
-            MyUser CountLetters = new MyUser();
-            CountLetters.getCounter();
-            MyUser countWords = new MyUser();
-            countWords.getWords();
-            MyUser longestWord = new MyUser();
-            longestWord.getLongestWord();
-            MyUser yourWord = new MyUser();
-            yourWord.getYourWords();
-
+            startCount.theCounter(text);
 
         }
 
-            System.out.println("Det här är totalt antal rader: " + startCount.getRows());
-            System.out.println("totalt antal tecken: " + startCount.getCounter());
-            System.out.println("totalt antal ord " + startCount.getWords());
-            System.out.println("längsta ordet " + startCount.getLongestWord());
-            System.out.println("och det här är alla dina ord: " + startCount.getYourWords());
 
-
-
+        System.out.println("Det här är totalt antal rader: " + startCount.getRows());
+        System.out.println("totalt antal tecken: " + startCount.getCounter());
+        System.out.println("totalt antal ord " + startCount.getWords());
+        System.out.println("längsta ordet " + startCount.getLongestWord());
+        System.out.println("och det här är alla dina ord: " + startCount.getYourWords());
 
 
     }
